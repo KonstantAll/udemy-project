2d60b7c94f72d170d063d11908a566968edcc16a
Elika Filin <elika.filin@pixelsmatter.com>
2023-10-17 14:47:42 +0300

    CommonJS project restructuring (lesson 103)

---
+++ js/modules/modal.js
@@ -0,0 +1,47 @@
+function modal(){
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal');
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    modal.addEventListener('click', (e) => {
+        if(e.target === modal || e.target.getAttribute('data-close') === '') {
+            closeModal();
+        }
+    });
+
+    function openModal() {
+        // modal.classList.toggle('show');
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
+    function closeModal(){
+        // modal.classList.toggle('show');
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+
+    document.addEventListener('keydown', (e) => {
+        if(e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
+    const modalTimerId = setTimeout(openModal, 50000);
+
+    function showModalByScroll() {
+        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+}
+module.exports = modal;
